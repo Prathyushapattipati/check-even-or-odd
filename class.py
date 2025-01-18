f3ebdc178f36d3813c128a55cff5611865cd5d39
Prathyushapattipati <pattipatisaivenkataprathyusha@gmail.com>
2025-01-18 16:30:51 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,7 @@
+def checkEvenorOdd():
+    a=int(input("Enter a number"))
+    if(a%2==0):
+        print("The entered number is Even number")
+    else:
+        print("The entered number is odd number")
+checkEvenorOdd()
